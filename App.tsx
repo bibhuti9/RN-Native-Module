f1902ec05c58129981a320226299784f0dddc050
bibhutiswain9 <144211577+bibhutiswain9@users.noreply.github.com>
2024-01-29 00:17:18 +0530

    successfully add native module

--- App.tsx
+++ App.tsx
@@ -1,10 +1,58 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  NativeModules,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-export default function App() {
+const {WebViewModule} = NativeModules;
+
+const App = () => {
+  const [convertToString, setConvertToString] = useState('');
+  const html = `<h1>Hey this is react  native</h1>`;
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{html}</Text>
+      <Text style={styles.text}>After HTML to String : {convertToString}</Text>
+
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => {
+          WebViewModule.convertHTML(html, (result: string) => {
+            setConvertToString(result);
+          });
+        }}>
+        <Text style={styles.btnText}>Convert to String</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  btn: {
+    marginTop: 40,
+    backgroundColor: '#e4aa3f',
+    borderRadius: 30,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  btnText: {
+    fontSize: 18,
+    color: '#fff',
+  },
+});
---
+++ android/app/src/main/java/com/rnnativemodule/WebViewModule.java
@@ -0,0 +1,32 @@
+package com.rnnativemodule;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.Html;
+import android.util.Log;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import java.util.Random;
+
+public class WebViewModule extends ReactContextBaseJavaModule {
+
+  public WebViewModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  public String getName() {
+    return "WebViewModule";
+  }
+
+  @ReactMethod
+  public void convertHTML(String html, com.facebook.react.bridge.Callback callback) {
+      CharSequence output = Html.fromHtml(html);
+      String htmlOutput = output.toString();
+      callback.invoke(htmlOutput);
+  }
+}
